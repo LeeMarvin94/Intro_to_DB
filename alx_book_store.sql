c4bfc546fe32e40f4db5df934c184ca42ef8e7c7
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2024-07-26 13:17:40 +0100

    Fixing bugs in task 0

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -43,7 +43,7 @@ CREATE TABLE Order_Details(
 	orderdetail_id INT PRIMARY KEY,
 	order_id INT,
 	book_id INT,
-	quantity FLOAT,
+	quantity DOUBLE,
 	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
 	FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
