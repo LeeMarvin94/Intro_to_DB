4ef760d470efb2f6f43ce5fe1cbe19bc2dd5d566
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2024-07-26 12:47:48 +0100

    Fixing bugs in task 0

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -35,7 +35,7 @@ CREATE TABLE Orders(
 	order_id INT PRIMARY KEY,
 	customer_id INT,
 	order_date DATE
-	FOREIGN KEY (customer_id) REFERENCES Customers(cusomer_id)
+	FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Table: Order_Details
