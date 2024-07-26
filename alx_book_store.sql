4d3f51046fcf8e9522560b2da2e28ce73a4b378a
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2024-07-26 12:25:14 +0100

    Adding the task 0

---
+++ alx_book_store.sql
@@ -0,0 +1,41 @@
+-- Mysql script for ALX project learning purposes
+
+-- Table: Books 
+CREATE TABLE Books(
+	book_id INT PRIMARY KEY,
+	title VARCHAR(130),
+	author_id INT,
+	price FLOAT,
+	publication_date DATE
+);
+
+-- Table: Stores
+CREATE TABLE Authors(
+	author_id INT PRIMARY KEY,
+	author_name VARCHAR(215)
+);
+
+-- Table: Customers
+CREATE TABLE Customers(
+	customer_id INT PRIMARY KEY,
+	customer_name VARCHAR(215),
+	email VARCHAR(215),
+	address TEXT
+);
+
+-- Table: Orders
+CREATE TABLE Orders(
+	order_id INT PRIMARY KEY,
+	customer_id INT,
+	order_date DATE
+);
+
+-- Table: Order_Details
+CREATE TABLE Order_Details(
+	orderdetail_id INT PRIMARY KEY,
+	order_id INT,
+	book_id INT,
+	quantity FLOAT
+);
+
+
