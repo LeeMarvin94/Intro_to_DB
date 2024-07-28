c37a825fa0aca8cbb6814f18dac2ddeacf33a619
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2024-07-28 13:30:41 +0100

    Adding task 4

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+SELECT 
+    COLUMN_NAME AS 'Column Name', 
+    COLUMN_TYPE AS 'Column Type', 
+    IS_NULLABLE AS 'Is Nullable', 
+    COLUMN_KEY AS 'Column Key', 
+    COLUMN_DEFAULT AS 'Column Default', 
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
+
